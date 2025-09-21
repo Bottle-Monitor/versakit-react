2e5b0db4de534fbf54367477e483928e5ce43219
resilience <547471919@qq.com>
2025-09-21 15:24:03 +0800

    fix: adds an explicit return type annotation RenderResult to the customRender function

--- packages/versakit-components/src/__tests__/test-utils.tsx
+++ packages/versakit-components/src/__tests__/test-utils.tsx
@@ -1,8 +1,8 @@
-import { type RenderOptions, render } from "@testing-library/react"
+import { type RenderOptions, type RenderResult, render } from "@testing-library/react"
 import type React from "react"
 
 // Custom render function that includes providers
-const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, "wrapper">) => {
+const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, "wrapper">): RenderResult => {
 	// You can add providers here in the future if needed
 	// const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
 	//   return <div>{children}</div>
