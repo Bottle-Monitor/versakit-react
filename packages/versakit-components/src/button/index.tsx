e64e9d0611ed21fc9d34f11ae1893a03c1b2d6ed
resilience <547471919@qq.com>
2025-09-23 13:43:59 +0800

    feat: 移除测试代码，保留方案

--- packages/versakit-components/src/button/index.tsx
+++ packages/versakit-components/src/button/index.tsx
@@ -3,7 +3,17 @@ import type { ButtonProps } from "./button.types"
 import { buttonVariants } from "./button.variants"
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, size, ...props }, ref) => {
+	({ className, variant, size, asChild = false, ...props }, ref) => {
+		const _Comp = asChild ? React.Fragment : "button"
+		const _buttonProps = asChild ? {} : { ref, ...props }
+
+		if (asChild) {
+			return React.cloneElement(props.children as React.ReactElement, {
+				className: buttonVariants({ variant, size, class: className }),
+				...props,
+			})
+		}
+
 		return <button className={buttonVariants({ variant, size, class: className })} ref={ref} {...props} />
 	},
 )
