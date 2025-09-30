b72b649597251e4939f8345b25f362f27e6d84cb
resilience <547471919@qq.com>
2025-10-01 00:45:52 +0800

    feat(Button): add loading state handling for asChild prop in Button component

--- packages/versakit-components/src/button/index.tsx
+++ packages/versakit-components/src/button/index.tsx
@@ -9,6 +9,15 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 	({ className, variant, size, asChild = false, loading = false, disabled, children, ...props }, ref) => {
 		const Comp = asChild ? Slot : "button"
 
+		// 当使用 asChild 时，不支持 loading 状态，因为 Slot 只允许单个子元素
+		if (asChild) {
+			return (
+				<Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+					{children}
+				</Comp>
+			)
+		}
+
 		return (
 			<Comp
 				className={cn(buttonVariants({ variant, size, className }))}
