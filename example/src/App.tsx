27b524cfc7f1c2b8fe5ee40bcf2c6829b0d1ffb6
resilience <547471919@qq.com>
2025-10-01 00:25:45 +0800

    fix: tailwindcss not config source

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import { Button } from "@versakit/components"
+
+function App() {
+	return (
+		<>
+			<div className="text-2xl font-bold text-red-500">Tailwind CSS is working</div>
+			<Button>Click me</Button>
+		</>
+	)
+}
+
+export default App
